b247914622214c4a4293b7ebfee06605243dad51
Kirill Redin <redinkirill.ua@gmail.com>
2019-07-25 22:19:25 +0300

    Solved problem 3. Count prime factor of a big number

---
+++ problem_3.py
@@ -0,0 +1,39 @@
+"""
+Description:
+	This program calculates the largest prime factor
+	of the number
+Computations:
+	prime factor = 0
+	factor = 2
+	while number > 1:
+		if number % factor == 0:
+			number = number / factor
+			if prime factor < factor:
+				prime factor = factor
+			factor = 2
+		else:
+			factor++
+"""
+
+def main():
+	number = int(input("Enter the number: "))
+	prime_factor = get_prime_factor(number)
+	print(prime_factor)
+
+def get_prime_factor(number):
+	prime_factor = 0
+	factor = 2
+
+	while number > 1:
+		if number % factor == 0:
+			number /= factor			
+			if prime_factor < factor:
+				prime_factor = factor
+			factor = 2
+		else:
+			factor += 1
+	return prime_factor
+
+
+if __name__ == '__main__':
+	main()
