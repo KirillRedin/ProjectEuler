e2515347213e23868dca418b5cfd182269218da2
Kirill Redin <redinkirill.ua@gmail.com>
2019-07-27 13:54:31 +0300

    Small changes in input and output.

--- problem_1.py
+++ problem_1.py
@@ -4,15 +4,19 @@ of 3 or 5 below 1000
 """
 
 def main():
-	bounds = tuple(input("Enter bounds: "))
-	count_multiplies_sum(bounds)
+	print("Calculate the sum of all the multiplies of 3 or 5 in an interval")
+	largest_num = int(input("Enter the largest number: "))
+	lowest_num = int(input("Enter the lowest number: "))
+	sum = count_multiplies_sum(largest_num, lowest_num)
+	print("The sum of all the multiplies of 3 or 5 in the [{}, {}] interval "\
+		"is {}".format(lowest_num, largest_num, sum))
 
-def count_multiplies_sum(bounds):
+def count_multiplies_sum(largest_num, lowest_num):
 	sum = 0 
-	for number in range(bounds[0], bounds[1]):
+	for number in range(lowest_num, largest_num + 1):
 		if number % 3 == 0 or number % 5 == 0:
 			sum += number
-	print(sum)
+	return sum
 
 if __name__ == '__main__':
 	main()
