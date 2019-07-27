a58de01668bc3d031ca26cb881182512128e1b1b
Kirill Redin <redinkirill.ua@gmail.com>
2019-07-27 18:56:45 +0300

    Added less complex algorithm. It uses one formula without traversing each value in sequence. Fixed function names.

--- problem_1.py
+++ problem_1.py
@@ -1,17 +1,33 @@
 """
-This program calculates the sum of all the multiplies
+This program calculates the sum of all the multiples
 of 3 or 5 below 1000
 """
 
 def main():
-	print("Calculate the sum of all the multiplies of 3 or 5 in a range")
+	print("Calculate the sum of all the multiples of 3 or 5 in a range")
 	largest_num = int(input("Enter the largest number: "))
 	lowest_num = int(input("Enter the lowest number: "))
-	sum = count_multiplies_sum(largest_num, lowest_num)
-	print("The sum of all the multiplies of 3 or 5 in the [{}, {}] interval "\
+	
+	sum_of_3 = count_multiples_sum(largest_num, lowest_num, 3)
+	sum_of_5 = count_multiples_sum(largest_num, lowest_num, 5)
+	# Count sum of multiples of 15 to exclude repeating numbers
+	sum_of_15 = count_multiples_sum(largest_num, lowest_num, 15)
+	
+	sum = sum_of_3 + sum_of_5 - sum_of_15
+	print("The sum of all the multiples of 3 or 5 in the [{}, {}] interval "\
 		"is {}".format(lowest_num, largest_num, sum))
 
-def count_multiplies_sum(largest_num, lowest_num):
+def count_multiples_sum(largest_num, lowest_num, number):
+	all_multiples_amount = largest_num // number
+	excluded_multiples_amount = (lowest_num - 1) // number
+	multiples_amount = all_multiples_amount - excluded_multiples_amount
+	max_multiple = all_multiples_amount * number
+	min_multiple = (excluded_multiples_amount + 1) * number
+	multiples_sum = (min_multiple + max_multiple) * multiples_amount / 2
+	return multiples_sum
+
+# Deprecated because of complexity
+def count_multiples_sum_depr(largest_num, lowest_num):
 	sum = 0 
 	for number in range(lowest_num, largest_num + 1):
 		if number % 3 == 0 or number % 5 == 0:
